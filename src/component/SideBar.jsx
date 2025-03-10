d4bb183a8ac138ef5d06327ade717bf1f3e72570
Adelani <adeoyejoel121@gmail.com>
2025-03-10 22:06:29 +0100

    Update SideBar.jsx
    Flex-flow

--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -3,13 +3,13 @@ import { Users } from '../constant/Data';
 
 const SideBar = () => {
   return (
-    <div className='flex flex-col justify-between w-[500px] border-yellow-50 max-h-screen border-r-2 shadow-2xl md:w-full lg:w-auto relative'>
+    <div className='flex  flex-col justify-between min-w-[500px] border-yellow-50 max-h-screen border-r-2 shadow-2xl md:w-full lg:flex-1 relative'>
 <div className="p-4 flex justify-between items-center">
         <img className='shadow-lg shadow-gray-600 w-8 bg-white border-none rounded-full p-1 cursor-pointer' src={backIcon} alt="" />
       </div>
 
     
-      <div className='flex-1 overflow-y-auto p-3 border-b-2 md:w-full lg:w-auto hide-scrollbar'>
+      <div className='flex flex-1 overflow-y-auto p-3 border-b-2 md:w-full lg:w-auto hide-scrollbar'>
         {Users.map((users, id) => (
           <div key={id} className="border-b-2 py-6 hover:bg-gray-100 cursor-pointer transition duration-100 flex items-start justify-between gap-4">
             <div className='flex items-center gap-5'>
