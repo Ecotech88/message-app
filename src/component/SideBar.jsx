1626fc615b211293624c75b94a425d653c7f6fb3
Adelani <adeoyejoel121@gmail.com>
2025-03-11 16:27:13 +0100

    Update SideBar.jsx
    Flex

--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -9,17 +9,17 @@ const SideBar = () => {
       </div>
 
     
-      <div className='flex-1 overflow-y-auto p-3 border-b-2 md:w-full lg:w-auto hide-scrollbar'>
+      <div className='sm:flex-1 overflow-y-auto p-3 border-b-2 md:w-full lg:w-auto hide-scrollbar'>
         {Users.map((users, id) => (
-          <div key={id} className="border-b-2 py-6 hover:bg-gray-100 cursor-pointer transition duration-100 flex items-start justify-between gap-4">
+          <div key={id} className="border-b-2 py-3 hover:bg-gray-100 cursor-pointer transition duration-100 flex items-start justify-between gap-4">
             <div className='flex items-center gap-5'>
-              <img className='size-12 object-cover rounded-full' src={users.images} alt="" />
+              <img className='min-size-12 object-cover rounded-full' src={users.images} alt="" />
               <div className='flex-col justify-center'>
                 <h2 className='font-bold text-xl'>{users.name}</h2>
-                <p className='text-base'>{users.greeting}</p>
+                <p className='text-8 '>{users.greeting}</p>
               </div>
             </div>
-            <span className="bg-purple-500 rounded-full px-3 text-white text-10 text-center my-2 font-semibold">
+            <span className="bg-purple-500 rounded-full px-1 text-white text-8 text-center my-2 font-semibold">
               LOOKING
             </span>
           </div>
